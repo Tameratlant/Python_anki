ee9d6d67cc908644e8fd58039e5ae69b200e972c
Tameratlant <fatykhov.tf@phystech.edu>
2025-04-12 00:42:41 +0300

    card adding page done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
@@ -25,15 +25,24 @@ class Card(db.Model):
 
 # Форма для добавления карточки
 class AddCardForm(FlaskForm):
-    front_text = StringField('Загаданное слово', validators=[DataRequired()])
-    back_text = StringField('Ответ', validators=[DataRequired()])
-    tag = StringField('Тег', validators=[DataRequired()])
-    color = SelectField('Цвет карточки', choices=[
-        ('red', 'Красный'),
-        ('blue', 'Синий'),
-        ('green', 'Зелёный'),
-        ('yellow', 'Жёлтый')
-    ], validators=[DataRequired()])
+    front_text = StringField('Загаданное слово', 
+                           validators=[DataRequired()],
+                           render_kw={"placeholder": "Введите загаданное слово"})
+    back_text = StringField('Ответ',
+                          validators=[DataRequired()],
+                          render_kw={"placeholder": "Введите правильный ответ"})
+    tag = StringField('Тег',
+                     validators=[DataRequired()],
+                     render_kw={"placeholder": "Например: 'глаголы', 'еда'"})
+    color = SelectField('Цвет карточки', 
+                       choices=[
+                           ('red', 'Красный'),
+                           ('blue', 'Синий'),
+                           ('green', 'Зелёный'),
+                           ('yellow', 'Жёлтый')
+                       ], 
+                       default='red',
+                       validators=[DataRequired()])
     submit = SubmitField('Добавить карточку')
 
 @app.route('/')
@@ -43,6 +52,7 @@ def index():
 @app.route('/add_cards', methods=['GET', 'POST'])
 def add_cards():
     form = AddCardForm()
+    
     if form.validate_on_submit():
         new_card = Card(
             front_text=form.front_text.data,
@@ -52,22 +62,34 @@ def add_cards():
         )
         db.session.add(new_card)
         db.session.commit()
-        return redirect(url_for('index'))
+        
+        # Для AJAX-запросов
+        if request.headers.get('Accept') == 'application/json':
+            return jsonify({'success': True})
+        
+        # Для обычной отправки формы (если отключен JavaScript)
+        flash('Карточка успешно добавлена!', 'success')
+        return redirect(url_for('add_cards'))
+    
     return render_template('add_card.html', form=form)
+
+@app.route('/my_cards')
+def my_cards():
+    cards = Card.query.order_by(Card.created_at.desc()).all()
+    return render_template('my_cards.html', cards=cards)
+
+@app.route('/guess_by_tags')
+def guess_by_tags():
+    tags = db.session.query(Card.tag.distinct()).all()
+    tags = [tag[0] for tag in tags]
+    return render_template('guess_by_tags.html', tags=tags)
+
+@app.route('/guess_all')
+def guess_all():
+    cards = Card.query.all()
+    return render_template('guess_all.html', cards=cards)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Создаём таблицы в БД
     app.run(debug=True)
-
-class AddCardForm(FlaskForm):
-    front_text = StringField('Загаданное слово', validators=[DataRequired()], render_kw={"placeholder": "Введите загаданное слово"})
-    back_text = StringField('Ответ', validators=[DataRequired()], render_kw={"placeholder": "Введите правильный ответ"})
-    tag = StringField('Тег', validators=[DataRequired()], render_kw={"placeholder": "Например: 'глаголы', 'еда'"})
-    color = SelectField('Цвет карточки', choices=[
-        ('red', 'Красный'),
-        ('blue', 'Синий'),
-        ('green', 'Зелёный'),
-        ('yellow', 'Жёлтый')
-    ], default='red', validators=[DataRequired()])
-    submit = SubmitField('Добавить карточку')
