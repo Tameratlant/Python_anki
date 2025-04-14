88e349dd31098645935e6b3a84d3c77085bba227
Tameratlant <fatykhov.tf@phystech.edu>
2025-04-14 22:43:36 +0300

    Tag learning done

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 from datetime import datetime
 import random
+from flask_session import Session
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cards.db'
 app.config['SECRET_KEY'] = 'your-secret-key-here'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(app)
+
 
 db = SQLAlchemy(app)
 
@@ -77,12 +82,6 @@ def my_cards():
     cards = Card.query.order_by(Card.created_at.desc()).all()
     return render_template('my_cards.html', cards=cards)
 
-@app.route('/guess_by_tags')
-def guess_by_tags():
-    tags = db.session.query(Card.tag.distinct()).all()
-    tags = [tag[0] for tag in tags]
-    return render_template('guess_by_tags.html', tags=tags)
-
 @app.route('/edit_card/<int:card_id>', methods=['GET', 'POST'])
 def edit_card(card_id):
     card = Card.query.get_or_404(card_id)
@@ -111,10 +110,8 @@ def guess_all():
     ).all()
     
     if not cards:
-        # Если все карточки угаданы, показываем сообщение
         return render_template('all_cards_learned.html')
     
-    # Выбираем случайную карточку из оставшихся
     card = random.choice(cards)
     return render_template('guess_all.html', card=card)
 
@@ -134,7 +131,71 @@ def handle_answer(card_id):
     
     return redirect(url_for('guess_all'))
 
+@app.route('/guess_by_tags', methods=['GET', 'POST'])
+def guess_by_tags():
+    if request.method == 'POST':
+        if not request.form.get('tag'):
+            return redirect(url_for('guess_by_tags'))
+            
+        selected_tag = request.form['tag']
+        selected_color = request.form.get('color', 'all')
+        
+        session['selected_tag'] = selected_tag
+        session['selected_color'] = selected_color
+        
+        query = Card.query.filter(Card.tag == selected_tag)
+        if selected_color != 'all':
+            query = query.filter(Card.color == selected_color)
+        
+        cards = query.all()
+        
+        if not cards:
+            flash('Нет карточек с выбранными параметрами', 'error')
+            return redirect(url_for('guess_by_tags'))
+        
+        session['cards_to_learn'] = [card.id for card in cards]
+        random.shuffle(session['cards_to_learn'])
+        
+        return redirect(url_for('practice_by_tags'))
+    
+    tags = db.session.query(Card.tag.distinct()).all()
+    tags = [tag[0] for tag in tags if tag[0]]
+    return render_template('select_tags.html', tags=tags)
+
+@app.route('/practice_by_tags')
+def practice_by_tags():
+    if 'cards_to_learn' not in session or not session['cards_to_learn']:
+        return redirect(url_for('guess_by_tags'))
+    
+    card_id = session['cards_to_learn'][0]
+    card = Card.query.get(card_id)
+    return render_template('practice_tags.html', card=card)
+
+@app.route('/handle_tags_answer/<int:card_id>', methods=['POST'])
+def handle_tags_answer(card_id):
+    card = Card.query.get_or_404(card_id)
+    is_correct = request.form.get('is_correct') == 'true'
+    
+    if is_correct:
+        card.correct_answers += 1
+        session['cards_to_learn'].remove(card.id)
+    else:
+        card.wrong_answers += 1
+        card.last_wrong_answer = datetime.utcnow()
+        session['cards_to_learn'].append(session['cards_to_learn'].pop(0))
+        random.shuffle(session['cards_to_learn'])
+    
+    db.session.commit()
+    
+    if not session['cards_to_learn']:
+        session.pop('cards_to_learn', None)
+        return render_template('tags_learned.html', 
+                            tag=session.get('selected_tag'),
+                            color=session.get('selected_color'))
+    
+    return redirect(url_for('practice_by_tags'))
+
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Создаём таблицы в БД
+        db.create_all()
     app.run(debug=True)
